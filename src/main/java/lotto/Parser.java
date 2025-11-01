277b2a1d3d0ef7a5d05f0d82f5b847339cc0e556
haeyoon1 <hynam1220@naver.com>
2025-11-01 19:28:04 +0900

    feat: 로또 문자열 -> List로 파싱하는 유틸 클래스 생성
    Parser
    - 문자열 -> List<String> -> List<Integer>로 파싱
    - parseToLotto 메서드 내부에서 parseToInt 메서드 호출

---
+++ src/main/java/lotto/Parser.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+
+    private static final String LOTTO_PARSER = ",";
+
+    public List<Integer> parseToLotto(String input) {
+        return Arrays.stream(input.split(LOTTO_PARSER))
+            .map(String::trim)
+            .filter(number -> !number.isBlank())
+            .map(this::parseToInt)
+            .toList();
+    }
+
+    private Integer parseToInt(String token) {
+        try {
+            return Integer.parseInt(token);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_FORMAT.getValue());
+        }
+    }
+
+}
