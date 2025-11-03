f7555b6edc8506f6c3ff86730ec4aff5248a8a03
haeyoon1 <hynam1220@naver.com>
2025-11-03 23:40:24 +0900

    refactor: 로또 번호 최대, 최소 값과 로또의 길이 상수화

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class LottoGenerator {
 
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
     public static List<Integer> generateLotto() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
     }
 }
