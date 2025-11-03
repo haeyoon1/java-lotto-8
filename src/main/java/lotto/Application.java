a4f3183c1e4bf4849961d27c4aa309db35bccd8b
haeyoon1 <hynam1220@naver.com>
2025-11-03 11:34:06 +0900

    refactor: Controller 실행 후 console 닫는 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.controller.LottoController;
 
 public class Application {
 
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.run();
+        try {
+            LottoController lottoController = new LottoController();
+            lottoController.run();
+        } finally {
+            Console.close();
+        }
     }
 }
