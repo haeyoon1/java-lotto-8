a56f7e7e636a197c757684acc4fd206459022a98
haeyoon1 <hynam1220@naver.com>
2025-11-03 10:27:10 +0900

    test: 금액을 저장하는 객체에 대한 단위테스트 추가 MoneyTest

---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.error.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+    @Test
+    @DisplayName("유효한 금액 입력 시 올바른 로또 개수가 계산된다.")
+    void 유효한_금액_입력_시_올바른_로또_개수가_계산된다() {
+        Money money = new Money("8000");
+
+        assertThat(money.getLottoCount()).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 구입금액이 입력되면 예외가 발생한다.")
+    void 숫자가_아닌_구입금액이_입력되면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Money("aaa"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_INPUT_NUMBER_FORMAT.getValue());
+    }
+
+    @Test
+    @DisplayName("1000원 단위가 아닌 구입금액이 입력되면 예외가 발생한다.")
+    void 구입금액의_단위가_1000원이_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Money("5500"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_MONEY_UNIT.getValue());
+    }
+}
